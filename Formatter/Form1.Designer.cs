71c49eba3100ebbe970feb5637a05e89d1ceea40
Garry Turner <garry@evolving.technology>
2021-10-12 15:53:22 +0100

    initial commit

---
+++ Formatter/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtInput.Location = new System.Drawing.Point(11, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(339, 426);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Location = new System.Drawing.Point(356, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(434, 12);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(339, 426);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Enabled = false;
+            this.btnCopy.Location = new System.Drawing.Point(356, 39);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 39);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "Copy output to clipboard";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 452);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ Formatter/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Formatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var inputText = txtInput.Text;
+            if (!ValidJson(inputText))
+            {
+                return;
+            }
+
+            if(inputText.StartsWith("["))
+            {
+                inputText = inputText.Remove(0, 1);
+            }
+
+            if (inputText.EndsWith("]"))
+            {
+                inputText = inputText.Remove(inputText.Length-1, 1);
+            }
+           
+            var pattern = @"\},";
+            var data = Regex.Split(inputText, pattern);
+
+            var outputText = "";
+            for (var i = 0; i < data.Length; i++)
+            {
+                var modifiedRecord = "";
+                modifiedRecord += data[i];
+                modifiedRecord = modifiedRecord.Replace(System.Environment.NewLine, "");
+                modifiedRecord = modifiedRecord.Replace(" ", "");
+                if (i != data.Length - 1)
+                {
+                    modifiedRecord += "}," + Environment.NewLine;
+                }
+
+                outputText += modifiedRecord;
+            }
+
+            outputText = $"[{outputText}]";
+            txtOutput.Text = outputText;
+            btnCopy.Enabled = !string.IsNullOrWhiteSpace(txtOutput.Text);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(txtOutput.Text))
+            {
+                Clipboard.SetText(txtOutput.Text);
+            }
+        }
+
+        public bool ValidJson(string s)
+        {
+            try
+            {
+                JToken.Parse(s);
+                return true;
+            }
+            catch (JsonReaderException ex)
+            {
+                Trace.WriteLine(ex);
+                return false;
+            }
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            btnConvert.Enabled = !string.IsNullOrWhiteSpace(txtInput.Text);
+        }
+    }
+}
